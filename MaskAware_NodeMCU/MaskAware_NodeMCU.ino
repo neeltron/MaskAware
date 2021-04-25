ed9706e5077390621df3520027a68a94561780a7
unknown <500075823@stu.upes.ac.in>
2021-04-25 13:55:22 +0530

    Bug Fixed: Indefinite Mask Health Score

--- MaskAware_NodeMCU/MaskAware_NodeMCU.ino
+++ MaskAware_NodeMCU/MaskAware_NodeMCU.ino
@@ -3,16 +3,18 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssid = "SSID";
-const char* password = "PASS";
+const char* ssid = "Trikuldham";
+const char* password = "jaihanuman1981";
 int sensorValue;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+unsigned long start, finished, elapsed;
 
 void setup() {
   Serial.begin(9600);
   mlx.begin();
   WiFi.begin(ssid, password);
-
+  
   while (WiFi.status() != WL_CONNECTED) {
 
     delay(1000);
@@ -22,12 +24,15 @@ void setup() {
 }
 
 void loop() {
+  start=millis();
   Serial.println(mlx.readObjectTempF());
-  sensorValue = analogRead(0);
-  Serial.println(sensorValue, DEC);
+  sensorValue = 1200000 - start;
+  int maskH = ((12000000 - start) / 1200000)*100;
+  String s = String(maskH);
+  Serial.println(s);
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
-    http.begin("http://maskaware.azurewebsites.net/entrypoint?username=neeltronzeta&air="+String(sensorValue)+"&ir="+String(mlx.readObjectTempF()));
+    http.begin("http://maskaware.azurewebsites.net/entrypoint?username=neeltron&air="+s+"&ir="+String(mlx.readObjectTempF()));
     int httpCode = http.GET();
     if (httpCode > 0) {
       String payload = http.getString();
@@ -36,4 +41,5 @@ void loop() {
     http.end();
   }
   delay(10000);
+  Serial.println(start);
 }
